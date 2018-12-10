44eb5d7ec1bab95e947422dc9fa958aea8afa625
Jaleesa McCrea <jmccrea01@gmail.com>
2018-12-09 23:40:59 -0500

    dictate

--- Test/index.js
+++ Test/index.js
@@ -17,4 +17,9 @@ icon.addEventListener('click', () => {
 
 const dictate = () => {
     recognition.start();
+    recognition.onresult = (event) => {
+        const speechToText = event.results[0][0].transcript;
+
+        paragraph.textContent = speechToText;
+    }
 }
